da966503dd0fba4c1549a6bc292a0817d725eda5
lolly1259998 <hadilbouhachem12@gmail.com>
2025-10-17 10:32:36 +0100

    Squashed commit of the following:
    commit ebd1de5aa3a5388654184a863d6c38a7ac113641
    Author: Nouunouu <119971836+Nouunouu@users.noreply.github.com>
    Date:   Fri Oct 17 00:08:11 2025 +0100
    
        updates frotn
    
    commit 08ee3c151f64a1a2f32116c44bac0426ab2943c5
    Author: Nouunouu <119971836+Nouunouu@users.noreply.github.com>
    Date:   Wed Oct 15 23:28:04 2025 +0100
    
        adding search and sort
    
    commit 39b7d1e79372303d03943e621a76664d3373ddc2
    Author: Nouunouu <119971836+Nouunouu@users.noreply.github.com>
    Date:   Wed Oct 15 23:07:40 2025 +0100
    
        Final Backoffice
    
    commit 7e7345488a5547c54b5d4a19d512577a3f120b50
    Author: Nouunouu <119971836+Nouunouu@users.noreply.github.com>
    Date:   Wed Oct 15 21:39:49 2025 +0100
    
        BackOffice and new dashbording updates
    
    commit 2f83cb1bdfbb6603851f977de6e9bbd6560e42aa
    Author: Nouunouu <119971836+Nouunouu@users.noreply.github.com>
    Date:   Wed Oct 15 21:21:47 2025 +0100
    
        BackOffice and new dashbording

---
+++ app/Models/Campaign.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Campaign extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'description', 'image', 'start_date', 'end_date', 'status', 'user_id'
+    ];
+
+    // Relation avec User (créateur de la campagne)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relation avec Participation
+    public function participations()
+    {
+        return $this->hasMany(Participation::class);
+    }
+}
