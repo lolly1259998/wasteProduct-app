0ad8257c01592216f6b965aad5f13517f16f1fe8
lolly1259998 <hadilbouhachem12@gmail.com>
2025-10-17 11:42:59 +0100

    waste

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< Updated upstream
 use App\Http\Controllers\AI\AIController;
 
 Route::post('/ai/predict', [AIController::class, 'predictWasteTreatment']);
-=======
 use App\Http\Controllers\Campaign\CampaignController;
 
 Route::apiResource('campaigns', CampaignController::class);
->>>>>>> Stashed changes
